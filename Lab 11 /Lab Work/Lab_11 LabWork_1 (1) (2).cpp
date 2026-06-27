0b588720db35a02445d948b774f1530c943ad83a
talha951535-creator <talha951535@gmail.com>
2026-06-27 10:43:28 +0500

    Add files via upload

---
+++ Lab 11 /Lab Work/Lab_11 LabWork_1 (1) (2).cpp
@@ -0,0 +1,21 @@
+// Name: Talha Basheer 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0751
+// Section: A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing a string variable
+    string message = "Hello Engineering World";
+
+    // Displaying the original message
+    cout << "Message: " << message << endl;
+
+    // Calculating and displaying the length of the string using built-in function
+    cout << "Length of string: " << message.length() << endl;
+
+    return 0;
+}
---
+++ Lab 11 /Lab Work/Lab_11 LabWork_2.cpp
@@ -0,0 +1,24 @@
+// Name: Talha Basheer 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0751
+// Section: A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing first name
+    string firstName = "Talha";
+
+    // Declaring and initializing last name
+    string lastName = "Basheer";
+
+    // Concatenating first name and last name with a space in between
+    string fullName = firstName + " " + lastName;
+
+    // Displaying the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}

c74a4e4306bf05df0ff8e07a8db0ef917be260da
talha951535-creator <talha951535@gmail.com>
2026-06-27 10:43:54 +0500

    Add files via upload

---
+++ Lab 12 /Lab Work/Lab_12 LabWork_1 (1).cpp
@@ -0,0 +1,26 @@
+// Name: Talha Basheer 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0751
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaring and initializing an integer variable
+    int num = 25;
+
+    // Declaring a pointer variable of type integer
+    int *ptr;
+
+    // Storing the address of num in pointer ptr
+    ptr = &num;
+
+    // Displaying the value of num directly
+    cout << "Value of num: " << num << endl;
+
+    // Displaying the value of num using pointer (dereferencing)
+    cout << "Value using pointer (*ptr): " << *ptr << endl;
+
+    return 0;
+}
---
+++ Lab 12 /Lab Work/Lab_12 LabWork_2 (1).cpp
@@ -0,0 +1,26 @@
+// Name: Talha Basheer 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0751
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaring and initializing an integer array
+    int numbers[] = {10, 20, 30, 40, 50};
+
+    // Declaring a pointer and pointing it to the first element of the array
+    int *ptr = numbers;
+
+    // Accessing and displaying array elements using pointer arithmetic
+    cout << "Array elements using pointer arithmetic:" << endl;
+
+    cout << "1st element: " << *ptr << endl;
+    cout << "2nd element: " << *(ptr + 1) << endl;
+    cout << "3rd element: " << *(ptr + 2) << endl;
+    cout << "4th element: " << *(ptr + 3) << endl;
+    cout << "5th element: " << *(ptr + 4) << endl;
+
+    return 0;
+}
